28019f41c866dd29b4bcdeecb0f91c96bbfec05f
leixin <1610151535@qq.com>
2018-10-18 20:25:30 +0800

    把本地工程添加到git

---
+++ aaa/src/com/lx/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.lx.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
